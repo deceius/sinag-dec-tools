857add788dab8be092329c775e5302438938963c
deceius <julius.decena3095@gmail.com>
2024-02-07 19:12:24 +0800

    Update RegearReportController.php

--- app/Http/Controllers/RegearReportController.php
+++ app/Http/Controllers/RegearReportController.php
@@ -30,7 +30,7 @@ class RegearReportController extends Controller
                 DB::raw("SUM(LENGTH(REPLACE(equipment, '!no_equip,', '')) - LENGTH(REPLACE(REPLACE(equipment, ',', ''), '!no_equip', '')) + 1) as unit"),
                 DB::raw('COUNT(1) as death_count'))
         ->groupBy('battle_id')
-        ->orderBy('created_at', 'desc')
+        ->orderBy('battle_id', 'desc')
         ->paginate(5);
         return $result;
     }
